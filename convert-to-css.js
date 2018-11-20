65bfe22dfa72c8b56da64ee7a0f86c84ed3ae2d1
Alex Zvorygin <grafetu@gmail.com>
2018-11-20 01:53:02 -0500

    ready for production use :^)

--- convert-to-css.js
+++ convert-to-css.js
@@ -1,6 +1,6 @@
 var fs = require('fs');
- 
-var contents = fs.readFileSync('output.log', 'utf8');
+
+var contents = fs.readFileSync('sicp_log.log', 'utf8');
 // console.log(contents);
 
 const lines = contents.split('\n')
@@ -11,11 +11,22 @@ const asPoints = cleaned.map(vals => vals.split(', '))
 
 
 const videoLengthSeconds = 17.02;
-console.log(asPoints.filter)
+// console.log(asPoints.filter)
 
 console.log('@keyframes animation {')
 
-const posToLine = (point, index, allPoints) => console.log(`    ${index/allPoints.length*100}%     {left: ${point[0]*100}%, top: ${point[1]*100}%}`)
+const posToLine = (point, index, allPoints) => {
+    const percent = index/allPoints.length*100
+    const left = point[0]*100
+    const top = point[1]*100
+    const lastFive = allPoints.slice(Math.max(0, index-2), index+1)
+    const leftAvg = lastFive.map(point => point[0]).reduce((acc, val) => (+acc)+(+val), 0) / 3 * 100;
+    const topAvg = lastFive.map(point => point[1]).reduce((acc, val) => (+acc)+(+val), 0) / 3 * 100;
+    // console.error(lastFive.map(point => point[0]).reduce((acc, val) => (+acc)+(+val), 0))
+    // console.log(`    ${percent}%     {left: ${left}%; top: ${top}%;}`)   
+    console.log(`[${percent/100},${leftAvg},${topAvg}],`)   
+    
+}
 
 asPoints.forEach(posToLine)
 
